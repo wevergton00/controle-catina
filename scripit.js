ee6aebafa1cf72d46a82e63d1e1d7502d56e4df0
wevergton00 <wevergton.sousa@escola.pr.gov.br>
2025-10-29 09:34:35 -0300

    Update scripit.js

--- scripit.js
+++ scripit.js
@@ -1,1 +1,10 @@
 
+if (true) {
+  var testeVar = "Sou var";
+  let testeLet = "Sou let";
+  console.log(testeVar); // Funciona
+  console.log(testeLet); // Funciona
+}
+
+console.log(testeVar); // Funciona!
+console.log(testeLet); // ERRO!
